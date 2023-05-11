13cb94f5f1743edebf8e7a64d5345f55900646b6
asilbek <bahromovasilbek9@gmail.com>
2023-05-11 12:13:41 +0500

    add provider and warehouse

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     parent = models.ForeignKey('Category', verbose_name='Kategoriyasi',
-                               null=True, blank=True, on_delete=models.PROTECT)
+                               null=True, blank=True, on_delete=models.PROTECT, related_name="child_product")
     name = models.CharField(verbose_name="Kategoriya nomi", max_length=255, null=False, blank=False)
 
     def __str__(self):
@@ -11,7 +11,8 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', verbose_name="Kategoriyasi", on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', verbose_name="Kategoriyasi",
+                                 on_delete=models.CASCADE, related_name='product_relate')
     name = models.CharField(verbose_name="Maxsulot nomi", max_length=250)
     full_name = models.CharField(verbose_name="Maxsulotning to'liq momi", max_length=255)
     price = models.DecimalField(verbose_name="Maxsulot narxi", max_digits=17, decimal_places=2)
--- product/serializers.py
+++ product/serializers.py
@@ -1,14 +1,29 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Category, Product
 
-class CategorySerializer(ModelSerializer):
+
+class ChildCategorySerializer(ModelSerializer):
 
     class Meta:
         model = Category
+        fields = "id", "name",
+
+class ChildProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
         fields = "__all__"
+
+class CategorySerializer(ModelSerializer):
+    child_category_obj = ChildCategorySerializer(source='child_product', many=True, read_only=True)
+    product_obj = ChildProductSerializer(source='product_relate', many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = "id", "name", "parent", "child_category_obj", "product_obj",
 
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
+
