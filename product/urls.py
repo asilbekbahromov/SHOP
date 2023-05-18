c298d3725c3661e6371ace35b689143f3d035229
asilbek <bahromovasilbek9@gmail.com>
2023-05-18 11:59:51 +0500

    change models,serializers,views,urls

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,16 @@
-from .views import CategoryViewSet, ProductViewSet
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter
+from django.urls import path, include
+from . import views
 
 router = SimpleRouter()
+router.register('character', views.CharacterViewSet)
+router.register('productcharacter', views.ProductCharactoryViewSet)
+router.register('product', views.ProductViewSet)
+router.register('country', views.CountryViewSet)
+router.register('productmedia', views.ProductMediaViewSet)
+router.register('productprice', views.ProductPriceViewSet)
+router.register('category', views.CategoryViewSet)
+router.register('manufactory', views.ManufactoryViewSet)
 
-router.register('categories', CategoryViewSet)
-router.register('products', ProductViewSet)
-
-urlpatterns =[
+urlpatterns = [
 ]
